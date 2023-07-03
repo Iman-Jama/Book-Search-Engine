0d2836a209b5bbd13473b3bf1bd8200a5fe8644e
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-07-03 21:18:21 +0100

    finished refactoring the code

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -11,6 +11,10 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { Link } from 'react-router-dom';
+
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -59,31 +63,33 @@ const SearchBooks = () => {
     }
   };
 
-  // create function to handle saving a book to our database
-  const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+const handleSaveBook = async (bookId) => {
+  const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+  if (!token) {
+    return false;
+  }
 
-    if (!token) {
-      return false;
+  const [saveBook,  { error }] = useMutation(SAVE_BOOK);
+
+  try {
+    const response = await saveBook({
+      variables: {
+        book: bookToSave,
+      },
+    });
+
+    if (response.error) {
+      throw new Error('Something went wrong!');
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
+    setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   return (
     <>
